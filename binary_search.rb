0a73521643f7a3c7a91ee32600b257748ad9c4d4
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-07-23 17:38:41 -0400

    Simplifica un poco más el código de búsqueda binaria.
    Más concretamente:
    - Tiene en cuenta desbordamiento de enteros aún sabiendo que
    este caso no es posible que suceda en Ruby. Esto es para que el estudiante
    pueda trasladar el algoritmo correctamente a otros lenguajes que sí permiten
    desbordamiento de enteros.
    - Agrega aserciones para probar el algoritmo.

--- binary_search.rb
+++ binary_search.rb
@@ -1,33 +1,35 @@
 # input = array siempre ordenado
 # input = target el entero que estamos buscando
 # output = el índice de la posición del target en el array.
-
 def binary_search(array, target)
-  return -1 if array.length == 0
-  array_length = array.length # 4
   lo = 0
-  hi = array_length - 1 # 3
-  iterate = true
-  while true
-    middle_index = (lo + hi) / 2
-    if array[middle_index] == target # 3 == 1? false
+  hi = array.length - 1 # arreglos en Ruby empiezan con índice 0
+  while lo <= hi
+    middle_index = lo + ((hi - lo) / 2)
+    if array[middle_index] == target
       return middle_index
-    elsif hi <= lo
-      return -1
-    elsif target < array[middle_index] # Descartamos el lado derecho || 1 < 3 true
-      hi = middle_index - 1 # [1, *]
-    elsif target > array[middle_index] # Descartamos el lado izquierdo
-      lo = middle_index + 1 # [*, 4]
+    end
+
+    if target < array[middle_index] # Descartamos la mitad derecha del arreglo
+      hi = middle_index - 1
+    elsif target > array[middle_index] # Descartamos la mitad izquierda del arreglo
+      lo = middle_index + 1
     end
   end
+
+  return -1
 end
 
-puts binary_search([1, 2, 3, 4], 1) == 0
-puts binary_search([1, 2, 3, 4], 2) == 1
-puts binary_search([1, 2, 3, 4], 3) == 2
-puts binary_search([1, 2, 3, 4], 4) == 3
-puts "-------"
-puts binary_search([], 4) == -1
-puts binary_search([1, 2, 4, 5, 6, 7, 8], 9) == -1
+# Tests
+def assert(expr)
+  raise "Assertion failed !" unless expr
+end
+
+assert binary_search([1, 2, 3, 4], 1) == 0
+assert binary_search([1, 2, 3, 4], 2) == 1
+assert binary_search([1, 2, 3, 4], 3) == 2
+assert binary_search([1, 2, 3, 4], 4) == 3
+assert binary_search([], 4) == -1
+assert binary_search([1, 2, 4, 5, 6, 7, 8], 9) == -1
 
 # By KaizenDevs
