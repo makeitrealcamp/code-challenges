768b84622f408e6e26f8d34a2f507f9b2bdf57e6
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-08-27 07:14:32 -0400

    Improves problem statement.

--- collatz_conjecture.js
+++ collatz_conjecture.js
@@ -1,20 +1,12 @@
 /*
-   3N + 1
-   
-   
-   if N es impar -> 3*N + 1
-   else -> N/2
-   
-   input: N,M | N < M
-   output: 
-   
-   1,2,3
-   
-   [1] -> 1
-   [2, 1] -> 2
-   [3, 10, 5, 16, 8, 4, 2, 1] -> 8
- */
 
+The 3n+1 problem is as follows: consider a positive number n. The cycle length of n is the number of times we repeat the following, until we reach n=1:
+If n is odd, then n ⇐ 3n+1
+If n is even, then n ⇐ n/2
+
+For example, given n=22, we see the following sequence: 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1. The cycle length of 22 is, therefore, 16, since it took 16 repetitions to get to 1.
+Given a definition of cycle length of a number, here’s the rest of the problem: given any two numbers i and j, compute the maximum cycle length over all numbers between i and j, inclusive.
+*/
 
 function isEven(n) {
    return (n % 2 === 0);
